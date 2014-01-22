64805a80665c12f9b8f99fdbd2b25e896005dcf8
Abhishek Reddy <abhishek.reddy@smxemail.com>
2014-01-22 17:12:18 +1300

    Bugfix choosing default coffee version

--- src/lein_coffee/plugin.clj
+++ src/lein_coffee/plugin.clj
@@ -1,6 +1,8 @@
 (ns lein-coffee.plugin
   (:require [lein-npm.plugin :as npm]))
 
+(def default-coffee-version ">=1.6")
+
 (defn hooks []
   (npm/hooks))
 
@@ -15,13 +17,14 @@
 (defn- ensure-coffee
   ([deps version]
      (let [coffee-matches (find-coffee-deps deps)
-           new-coffee-dep ["coffee-script" version]]
-      (if (empty? coffee-matches)
-        (conj deps new-coffee-dep)
-        (assoc-in deps (first coffee-matches) new-coffee-dep))))
+           new-coffee-dep ["coffee-script" (or version default-coffee-version)]]
+       (println (format "Adding npm dependency on %s" new-coffee-dep))
+       (if (empty? coffee-matches)
+         (conj deps new-coffee-dep)
+         (assoc-in deps (first coffee-matches) new-coffee-dep))))
   ([deps]
-     (ensure-coffee deps ">=1.6")))
+     (ensure-coffee deps default-coffee-version)))
 
 (defn middleware
   [project]
-  (update-in project [:node-dependencies] #(ensure-coffee % (:coffee-version project))))
+  (update-in project [:node-dependencies] #(vec (ensure-coffee % (get-in project [:coffee :version])))))
