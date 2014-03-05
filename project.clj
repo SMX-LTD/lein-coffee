a8302568f285d6200bb0fd07b4ab3a184bcc3d27
Abhishek Reddy <abhishek.reddy@smxemail.com>
2014-03-06 11:19:47 +1300

    Bump lein-npm dependency to 0.3.0

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/SMX-LTD"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[lein-npm "0.2.0"]
+  :dependencies [[lein-npm "0.3.0"]
                  [lein-shell "0.3.0"]]
   :eval-in-leiningen true)
